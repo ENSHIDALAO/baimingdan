560477d0f87544ed089d9e28b22f8c2cc92c672e
BEN <2321221466@qq.com>
2025-08-07 00:44:21 +0800

    Update BEN.lua

--- BEN.lua
+++ BEN.lua
@@ -3,10 +3,10 @@ local LocalPlayer = Players.LocalPlayer
 
 -- 白名单列表
 local whitelist = {
-    "YourUsernameHere",  -- 替换为你的用户名
+    "jduej119",  -- 替换为你的用户名
     "fifkchxuc",       -- 添加其他用户名
-    "4575",
-    ""        -- 最多支持50个用户名
+    "ddguyfdd",
+    "Snow_GJDW","46469gmmgwg","greenbag119",        -- 最多支持50个用户名
 }
 
 -- 检查白名单是否为空或无效
@@ -20,7 +20,6 @@ local function isWhitelistValid()
     return false
 end
 
--- 检查当前玩家是否在白名单中
 local function isPlayerWhitelisted(playerName)
     -- 先去除两边空格再比较
     playerName = playerName:gsub("^%s*(.-)%s*$", "%1")
