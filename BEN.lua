4d6601d656bd2ade91d09e8db281df767dd8b174
BEN <2321221466@qq.com>
2025-08-23 01:03:43 +0800

    Update BEN.lua

--- BEN.lua
+++ BEN.lua
@@ -4,7 +4,7 @@ local LocalPlayer = Players.LocalPlayer
 -- 白名单列表
 local whitelist = {
     "jduej119",  -- 替换为你的用户名
-    "9178666nb72",       -- 添加其他用户名
+    "",       -- 添加其他用户名
     "hdhdgh830",
     "chunai99y","46469gmmgwg","greenbag119","gjigdtud","vopls0",        -- 最多支持50个用户名
 }
